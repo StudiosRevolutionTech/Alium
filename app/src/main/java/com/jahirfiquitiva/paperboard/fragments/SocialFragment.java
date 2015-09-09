911dc695a35f9ff798da80612ee9880895b7bc2c
Hector Franco <hector19971997@gmail.com>
2015-09-08 22:13:15 -0500

    Suba mierda

--- app/src/main/java/com/jahirfiquitiva/paperboard/fragments/SocialFragment.java
+++ app/src/main/java/com/jahirfiquitiva/paperboard/fragments/SocialFragment.java
@@ -1,7 +1,25 @@
 package com.jahirfiquitiva.paperboard.fragments;
 
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.studiosrevtech.alium.R;
+
 /**
  * Created by Hector on 9/8/2015.
  */
-public class SocialFragment {
+public class SocialFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.section_credits, container, false);
+
+
+        return root;
+
+    }
+
 }
